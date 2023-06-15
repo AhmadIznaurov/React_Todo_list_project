2963a7a1567ca9e86742bb18e0b5467b62b28b6f
AhmadIznaurov <111181823+AhmadIznaurov@users.noreply.github.com>
2023-06-15 23:24:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { useState } from "react";
+import { Header } from "./Header";
+import { Forms } from "./Forms";
+import { Todos } from "./Todos";
+
 
 function App() {
+
+    const [todos, setTodos] = useState([
+        {favorite: false, text: 'Купиты продукты'},
+        {favorite: true, text: 'Купиты бананы'},
+        {favorite: false, text: 'Купиты машину'},
+        {favorite: true, text: 'Купиты дом'},
+        {favorite: false, text: 'Купиты участок'}
+    ])
+
+    const [text, setText] = useState('')
+
+    const deleteTodo = (indexOfDelete) => {
+     const filterTodo = todos.filter((todo, index) => {
+         if (index === indexOfDelete) {
+             return false
+         }
+         return true
+     })
+        setTodos(filterTodo)
+    }
+
+    const makeFavorite = (indexOfFavotite) => {
+        const newTodos = todos.map((item, index) => {
+            if (index === indexOfFavotite) {
+                return {
+                    ...item,
+                    favorite: !item.favorite
+                }
+            }
+            return item;
+        })
+        setTodos(newTodos);
+    }
+    const addTodo = (e) => {
+        e.preventDefault();
+        const noCopy = todos.find(todo => todo.text === text);
+        if (!noCopy) {
+            setTodos([{ text: text, favorite: false }, ...todos]);
+        }
+        setText('');
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Header />
+          <Forms
+              text={text}
+              setText={setText}
+              addTodo={addTodo}
+          />
+          <Todos
+              todos={todos}
+              makeFavorite={makeFavorite}
+              deleteTodo={deleteTodo}
+          />
     </div>
   );
 }
